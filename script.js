0fe22ea1ae939872ceffe8a846e3d50da3c92b36
= <ebrahemsamer2@gmail.com>
2019-05-30 22:04:15 +0300

    Search Button Click Event

--- script.js
+++ script.js
@@ -13,6 +13,19 @@ const removeAll = (className)=> {
 		removed[i].remove();
 	}
 };
+let appendErrorMessage = (studentContainer) => {
+	if(document.querySelector('.error') == undefined){
+		let errorMessage = document.createElement('p');
+		errorMessage.className = 'error';
+		errorMessage.innerHTML = "Can not Find This User";
+		studentContainer.appendChild(errorMessage);
+	}
+};
+let deleteErrorMessage = ()=> {
+	if(document.querySelector('.error') != undefined){
+		document.querySelector('.error').remove();
+	}
+}
 
 	window.addEventListener('load', ()=> {
 		const studentContainer = document.querySelector('#students');
@@ -63,10 +76,64 @@ const removeAll = (className)=> {
 						generateStudents(i, studentContainer);
 					}
 				}
+				deleteErrorMessage();
 			});
 
 
-	});
+const searchButton = document.querySelector('#searchButton');
 
-const seachButton = document.querySelector('#search button');
+searchButton.addEventListener('click', () => {
 
+	let inputValue = document.querySelector('#search input').value;
+
+	for (let i = 0; i < students.length; i++) {
+		
+		if(students[i].name.toLowerCase() == inputValue.toLowerCase()) {
+			// Student Found
+			removeAll('student');
+			let student = document.createElement('div');
+			student.className = 'student';
+			student.innerHTML = "<img class='student_image' width='50' height='50' src='images/" + students[i].image + " '>";
+			student.innerHTML += "<h2 class='student_name'>" + students[i].name + "</h2>";
+			student.innerHTML += "<h3 class='student_college'>" + students[i].college + "</h3>";
+			student.innerHTML += "<p class='student_join'>" + students[i].joined_at + "</p>";
+			studentContainer.appendChild(student);
+			deleteErrorMessage();
+			break;
+		}else{
+
+			if(inputValue.split(' ').length == 1){
+
+				if(students[i].name.split(' ')[0].toLowerCase() == inputValue.toLowerCase()) {
+					// Student Found
+					removeAll('student');
+					let student = document.createElement('div');
+					student.className = 'student';
+					student.innerHTML = "<img class='student_image' width='50' height='50' src='images/" + students[i].image + " '>";
+					student.innerHTML += "<h2 class='student_name'>" + students[i].name + "</h2>";
+					student.innerHTML += "<h3 class='student_college'>" + students[i].college + "</h3>";
+					student.innerHTML += "<p class='student_join'>" + students[i].joined_at + "</p>";
+					studentContainer.appendChild(student);
+					deleteErrorMessage();
+					break;
+				}else {
+					// Not Found
+					removeAll('student');
+					appendErrorMessage(studentContainer);
+					break;
+				}
+
+			} else {
+				removeAll('student');
+				appendErrorMessage(studentContainer);
+				break;
+			}
+
+		}
+
+	}
+
+});
+
+
+});
